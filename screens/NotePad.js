af088509284c7fd341115405998f71b1b3846b5c
manishcoder2212 <75921394+manishcoder2212@users.noreply.github.com>
2023-02-06 16:14:13 +0530

    Add files via upload

---
+++ screens/NotePad.js
@@ -0,0 +1,16 @@
+import *as React from 'react';
+import {View,Text,TouchableOpacity} from 'react-native'
+
+ 
+export default class NotePad extends React.Component{
+  render(){
+  return (
+   <>
+   <TouchableOpacity><Text>NotePad</Text></TouchableOpacity>
+   </>
+  );
+} 
+}
+
+
+
